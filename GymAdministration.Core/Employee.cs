e465bab230b9085eb7be15821f8c2340e2f93719
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-03-02 14:23:09 -0300

    class employee

--- GymAdministration.Core/Employee.cs
+++ GymAdministration.Core/Employee.cs
@@ -2,6 +2,27 @@
 {
     public class Employee
     {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public DateTime StartedDate { get; private set; }
+        public DateTime FinishedDate { get; private set; }
 
+        public StatusEnum Status { get; private set; }
+
+
+
+        public Employee(int id, string name, DateTime startedDate, DateTime finishedDate, StatusEnum status)
+        {
+            Random random = new Random();
+            Id = random.Next(50000, 100000);
+            Name = name;
+            StartedDate = startedDate;
+            FinishedDate = finishedDate;
+            Status = StatusEnum.Active;
+
+        }
     }
+
+
+
 }
