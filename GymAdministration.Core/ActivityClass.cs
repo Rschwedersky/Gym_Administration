c88ae0bab746d9b9962f778650fb7c9e173816de
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-03-02 16:14:27 -0300

    class activity

--- GymAdministration.Core/ActivityClass.cs
+++ GymAdministration.Core/ActivityClass.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace GymAdministration.Core
 {
-    internal class ActivityClass
+    public class ActivityClass
     {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime StartedDate { get; private set; }
+        public DateTime FinishedDate { get; private set; }
+
+        public StatusEnum Status { get; private set; }
+
+        public Employee Employee { get; private set; }
+
+        public Student Student { get; private set; }
+
+        public ActivityClass(int id, string name, string description, DateTime startedDate, DateTime finishedDate, StatusEnum status, Employee employee, Student student)
+        {
+            Random random = new Random();
+            Id = random.Next(50000, 100000);
+            Name = name;
+            Description = description;
+            StartedDate = startedDate;
+            FinishedDate = finishedDate;
+            Status = StatusEnum.Active;
+            Employee = employee;
+            Student = student;
+
+        }
     }
 }
